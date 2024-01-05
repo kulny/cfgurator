7c89c0e678c977fb38759f12eedeb1645de8104c
kulny <wordworthathousandpictures@gmail.com>
2024-01-05 14:39:40 -0500

    onclick working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Javacript comments
+
+document.addEventListener("DOMContentLoaded", function(event){
+    const keySelector = document.getElementById('key-selector');
+
+    keySelector.addEventListener('click', changeEditbox);
+});
+
+function changeEditbox(event){
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton){
+        return;
+    }
+    var editbox = document.getElementById('editbox');
+    editbox.value = event.target.innerHTML;
+}
