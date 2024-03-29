5f0a50dd88bf45463bd0eda8417729bdd067058e
kulny <wordworthathousandpictures@gmail.com>
2024-03-28 21:17:22 -0400

    added jquery

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
 let configuredData = {};
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  const keySelector = document.getElementById("key-selector");
-  // const saveButton = document.getElementById('save')
-  const dlButton = document.getElementById('download-button');
+  const keySelector = $("#key-selector");
+  // const saveButton = $('#save')
+  const dlButton = $("#download-button");
 
   let cfgDataDefault;
   fetch("./cfgDefault.json")
-    .then((response) => response.json()).then((json) => {
+    .then((response) => response.json())
+    .then((json) => {
       cfgDataDefault = json;
     });
 
-  keySelector.addEventListener("click", (event) => {
+  keySelector.on("click", (event) => {
     changeEditbox(event, cfgDataDefault);
   });
-  save.addEventListener("click", (event) => {
-    saveCfg();
-  });
+  // save.on("click", (event) => {
+  //   saveCfg();
+  // });
 
   addIDsToButtons();
 
-  dlButton.addEventListener('click', function () {
-    downloadCfg();
-
-  }, false);
+  dlButton.on(
+    "click",
+    function () {
+      downloadCfg();
+    },
+    false
+  );
 });
 
 function addIDsToButtons() {
-  for (let i = 0; i < document.querySelectorAll('button').length; i++) {
-    const button = document.querySelectorAll('button')[i];
-    if (button.innerHTML == '' || button.innerHTML.indexOf('arrow') != -1) {
+  for (let i = 0; i < $("key-selector button").length; i++) {
+    const button = $("key-selector button")[i];
+    if (button.innerHTML == "" || button.innerHTML.indexOf("arrow") != -1) {
       continue;
     }
 
-    button.setAttribute('id', button.innerHTML.toLowerCase());
+    button.attr("id", button.innerHTML.toLowerCase());
   }
 }
 
 function downloadCfg() {
-  var link = document.createElement('a');
-  link.setAttribute('download', 'autoexec.cfg');
+  var link = document.createElement("a");
+  link.setAttribute("download", "autoexec.cfg");
   link.href = makeCfgFile();
   document.body.appendChild(link);
 
   window.requestAnimationFrame(function () {
-    var event = new MouseEvent('click');
+    var event = new MouseEvent("click");
     link.dispatchEvent(event);
     document.body.removeChild(link);
   });
 }
 
-function saveCfg(){
-  var keyboardKey = document.getElementById('cfg-options').dataset.keyboardKey.toLowerCase();
-  var editbox = document.getElementById('editbox');
+function saveCfg() {
+  var keyboardKey = document
+    .getElementById("cfg-options")
+    .dataset.keyboardKey.toLowerCase();
+  var editbox = document.getElementById("editbox");
 
   configuredData[keyboardKey] = editbox.value;
-  document.getElementById(keyboardKey.toLowerCase()).classList.add('edited-button')
-  console.log(configuredData)
+  document
+    .getElementById(keyboardKey.toLowerCase())
+    .classList.add("edited-button");
+  console.log(configuredData);
 }
 
 function changeEditbox(event, json) {
@@ -68,10 +76,10 @@ function changeEditbox(event, json) {
   var key = event.target.innerHTML;
   console.log(json);
 
-  if (key.toLowerCase() == 'dev') {
-    var presetsSelector = document.getElementById('cfg-options');
-    var devSelect = document.createElement('option');
-    devSelect.innerHTML = 'devTest';
+  if (key.toLowerCase() == "dev") {
+    var presetsSelector = document.getElementById("cfg-options");
+    var devSelect = document.createElement("option");
+    devSelect.innerHTML = "devTest";
     presetsSelector.replaceChildren(devSelect);
     return;
   }
@@ -79,73 +87,67 @@ function changeEditbox(event, json) {
   buildDropdown(key, bindEditor, json);
 
   if (configuredData[event.target.innerHTML.toLowerCase()]) {
-    var bindEditBox = document.getElementById('editbox')
-    bindEditBox.innerHTML = configuredData[event.target.innerHTML.toLowerCase()];
+    var bindEditBox = document.getElementById("editbox");
+    bindEditBox.innerHTML =
+      configuredData[event.target.innerHTML.toLowerCase()];
   } else {
-    var bindEditBox = document.getElementById('editbox')
-    bindEditBox.innerHTML = '';
+    var bindEditBox = document.getElementById("editbox");
+    bindEditBox.innerHTML = "";
   }
 }
 
 function buildDropdown(keyboardKey, bindEditor, json) {
-  // replace select element if it exists
-  if (bindEditor.firstElementChild.nodeName === 'select') {
-    bindEditor.removeChild(bindEditor.firstElementChild);
-  }
-  // create select element
-  var presetsSelector = document.createElement("select");
-  presetsSelector.setAttribute("id", 'cfg-options' );
-  // add the select element if it is not already there
-  if(!bindEditor.contains(document.getElementById('cfg-options'))){
-    bindEditor.prepend(presetsSelector);
-  }
+  var presetsSelector = $("#cfg-options");
 
-  var defaultOption = document.createElement('option');
-  defaultOption.setAttribute('id', 'default');
-  defaultOption.innerHTML = '--- Presets ---';
-  defaultOption.value = '-1';
+  var defaultOption = $("<option id='default' value='-1'>--- Presets ---</option>");
+  console.log(defaultOption)
   var newChildren = [defaultOption];
+
+  presetsSelector.append($('<option>', {
+    id: "default",
+    value: -1,
+    text: "--- Presets ---",
+  }));
   // get the cfg options from json and make option elements from them
   for (var item in json[keyboardKey.toLowerCase()][0]) {
-
-    const option = document.createElement("option");
-    option.setAttribute('id', item);
-    option.innerHTML = item;
-    option.value = item;
-    
-    newChildren.push(option);
+    presetsSelector.append($("<option>", {
+      id: item,
+      value: item,
+      text: item,
+    }));
   }
-  // the 'current' presetsSelector could be the manufactured one which will not replace the one 
-  // that is already there, so we must redefine it as the one that is already in the DOM
-  presetsSelector = document.getElementById('cfg-options');
-  presetsSelector.replaceChildren(...newChildren);
-  presetsSelector.setAttribute('data-keyboard-key', keyboardKey);
+  presetsSelector.attr("data-keyboard-key", keyboardKey);
+  console.log(presetsSelector)
 
-  presetsSelector.onchange = (event) => {
+  // presetsSelector.onchange = (event) => {
+  //   populateEditbox(json);
+  // };
+  presetsSelector.on("change", () => {
     populateEditbox(json);
-  }
+  });
 }
 
-function populateEditbox(json){
-  var selector = document.getElementById('cfg-options');
-  console.log('popeditbox')
-  var bindEditBox = document.getElementById('editbox')
+function populateEditbox(json) {
+  var selector = $("#cfg-options");
+  console.log("popeditbox");
+  var bindEditBox = $("#editbox");
   console.log(selector.dataset.keyboardKey);
   if (selector.value == -1) {
-    bindEditBox.innerHTML = '';
+    bindEditBox.innerHTML = "";
   } else {
-    bindEditBox.innerHTML = json[selector.dataset.keyboardKey.toLowerCase()][0][selector.value];
+    bindEditBox.innerHTML =
+      json[selector.dataset.keyboardKey.toLowerCase()][0][selector.value];
   }
 }
 
 var cfgFile = null,
   makeCfgFile = function () {
-    var text = '';
-    Object.keys(configuredData).forEach(element => {
-      text += '## ' + element + '\n' + configuredData[element] + '\n';
+    var text = "";
+    Object.keys(configuredData).forEach((element) => {
+      text += "## " + element + "\n" + configuredData[element] + "\n";
     });
 
-    var data = new Blob([text], {type: 'text/plain'});
+    var data = new Blob([text], { type: "text/plain" });
 
     if (cfgFile !== null) {
       window.URL.revokeObjectURL(cfgFile);
@@ -154,4 +156,4 @@ var cfgFile = null,
     cfgFile = window.URL.createObjectURL(data);
 
     return cfgFile;
-  }
+  };
